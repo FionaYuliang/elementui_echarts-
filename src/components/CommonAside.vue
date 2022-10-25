65231f1c3d5bd38e21c14fe5c3e1912d11f57dbd
FionaYuliang <yangmingming94@gmail.com>
2022-10-25 19:31:47 +0800

    组件化侧边栏菜单

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -2,26 +2,37 @@
   <el-menu
     default-active="2"
     class="el-menu-vertical-demo"
-    @open="handleOpen"
-    @close="handleClose"
     background-color="#545c64"
     text-color="#fff"
     active-text-color="#ffd04b"
   >
-    <el-menu-item index="1">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
+    <el-menu-item
+      :index="item.path"
+      v-for="item in noChildren"
+      :key="item.path"
+    >
+      <i :class="'el-icon-' + item.icon"></i>
+      <span slot="title">{{ item.label }}</span>
     </el-menu-item>
 
-    <el-submenu index="2">
+    <el-submenu
+      :index="item.path"
+      v-for="(item, index) in hasChilren"
+      :key="index"
+    >
       <template slot="title">
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
+        <i :class="'el-icon-' + item.icon"></i>
+        <span>{{ item.label }}</span>
       </template>
       <el-menu-item-group>
-        <template slot="title">分组一</template>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
+        <el-menu-item
+          :index="subItem.path"
+          v-for="(subItem, subIndex) in item.children"
+          :key="subIndex"
+        >
+          <i :class="'el-icon-' + item.icon"></i>
+          <span slot="title"> {{ subItem.label }}</span>
+        </el-menu-item>
       </el-menu-item-group>
     </el-submenu>
   </el-menu>
@@ -31,10 +42,10 @@
 export default {
   computed: {
     noChildren() {
-      return this.menu.filter((item) => !item.children);
+      return this.asideMenu.filter((item) => !item.children);
     },
     hasChilren() {
-      return this.menu.filter((item) => item.children);
+      return this.asideMenu.filter((item) => item.children);
     },
   },
   data() {
@@ -59,7 +70,7 @@ export default {
           icon: "user",
         },
         {
-          label: "other",
+          label: "其他功能",
           icon: "setting",
           children: [
             {
@@ -79,14 +90,7 @@ export default {
       ],
     };
   },
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
-    },
-  },
+  methods: {},
 };
 </script>
 
