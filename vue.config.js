b9cb0ee441309ad94fb3955fe756a3104f1c3682
FionaYuliang <yangmingming94@gmail.com>
2022-10-25 18:46:43 +0800

    添加scss;注释报错片段

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,14 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 3333,
-    open: true,
+    open: false,
   },
+  // css: {
+  //   loaderOptions: {
+  //     sass: {
+  //       // 新版本sass-loader， 将data改成prependData进行配置
+  //       data: `@import "@/assets/scss/_variable.scss";`,
+  //     },
+  //   },
+  // },
 });
