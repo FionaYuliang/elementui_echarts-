038467f0ba71496bc68f0438c934c946b5efaa91
FionaYuliang <yangmingming94@gmail.com>
2022-10-26 11:24:19 +0800

    完成首页除图标部分的内容

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,18 +1,39 @@
 <template>
   <el-row class="home" :gutter="20">
     <el-col :span="8">
-      <el-card shadow="hover" style="height: 290px">section 1</el-card>
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="userAvatar" />
+          <div class="userinfo">
+            <p class="name">杨明明</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间：<span>2022-08-01</span></p>
+          <p>上次登录地点：<span>北京市海淀区</span></p>
+        </div>
+      </el-card>
       <el-card shadow="hover" style="height: 522px; margin-top: 20px"
         >section 2</el-card
       >
     </el-col>
     <el-col :span="16">
       <div class="num">
-        <el-card shadow="hover" v-for="item in 6" :key="item">
-          <i class="icon"></i>
+        <el-card
+          shadow="hover"
+          v-for="item in countData"
+          :key="item.name"
+          :body-style="{ display: 'flex', padding: 0 }"
+        >
+          <i
+            class="icon"
+            :class="`el-icon-${item.icon}`"
+            :style="{ background: item.color }"
+          ></i>
           <div class="detail">
-            <p class="num">money</p>
-            <p class="txt">description</p>
+            <p class="num">￥ {{ item.value }}</p>
+            <p class="txt">{{ item.name }}</p>
           </div>
         </el-card>
       </div>
@@ -27,6 +48,49 @@
 
 <script>
 export default {
+  data() {
+    return {
+      userAvatar: require("../assets/images/user.png"),
+      countData: [
+        {
+          name: "今日支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "今日收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "今日未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+        {
+          name: "本月支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "本月收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "本月未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+      ],
+    };
+  },
   mounted() {
     this.$http.get("/home/getData").then((res) => {
       console.log(res.data);
@@ -38,6 +102,39 @@ export default {
 <style lang="scss" scoped>
 // @import "~@assets/scss/home.scss";
 .home {
+  .user {
+    display: flex;
+    align-items: center;
+    padding-bottom: 20px;
+    margin-bottom: 20px;
+    border-bottom: 1px solid #ccc;
+    img {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      margin-right: 40px;
+    }
+    &info {
+      .name {
+        font-size: 32px;
+        margin-bottom: 40px;
+      }
+      .access {
+        color: #999999;
+      }
+    }
+  }
+  .login-info {
+    p {
+      line-height: 28px;
+      font-size: 14px;
+      color: #999999;
+      span {
+        color: #6666;
+        margin-left: 60px;
+      }
+    }
+  }
   .num {
     display: flex;
     flex-wrap: wrap;
@@ -45,6 +142,29 @@ export default {
     .el-card {
       width: 32%;
       margin-bottom: 20px;
+    }
+    .icon {
+      font-size: 30px;
+      width: 80px;
+      height: 80px;
+      text-align: center;
+      line-height: 80px;
+      color: #fff;
+    }
+    .detail {
+      margin-left: 15px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      .num {
+        font-size: 30px;
+        margin-bottom: 10px;
+      }
+      .txt {
+        font-size: 14px;
+        text-align: center;
+        color: #999999;
+      }
     }
   }
   .graph {
