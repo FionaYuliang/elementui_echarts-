6502f58fb273273005ebf95e1a348742d6d88a52
FionaYuliang <yangmingming94@gmail.com>
2022-10-26 20:49:58 +0800

    创建用户管理页面结构，表单组件封装

---
+++ src/components/CommonForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <el-form :inline="true" :model="form" ref="form" class="demo-form-inline">
+    <el-form-item
+      v-for="item in formLabel"
+      :key="item.model"
+      :label="item.label"
+    >
+      <el-input
+        v-model="form[item.model]"
+        placeholder="请输入内容"
+        v-if="item.type === 'input'"
+      ></el-input>
+      <el-select
+        v-model="form[item.model]"
+        placeholder="请选择"
+        v-if="item.type === 'select'"
+      >
+        <el-option
+          v-for="item in item.options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+      <el-switch
+        v-model="form[item.model]"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+        v-if="item.type === 'switch'"
+      >
+      </el-switch>
+      <el-date-picker
+        v-model="form[item.model]"
+        type="date"
+        placeholder="选择日期"
+        value-format="yyyy-MM-dd"
+        v-if="item.type === 'date'"
+      >
+      </el-date-picker>
+    </el-form-item>
+    <el-form-item><slot></slot></el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  props: {
+    inline: Boolean,
+    form: Object,
+    formLabel: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/UserManage/UserManage.vue
+++ src/views/UserManage/UserManage.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>user manage</div>
+  <div class="manage">
+    <div class="manage-header">
+      <common-form :formLabel="formLabel" :form="searchFrom">
+        <el-button type="primary">搜索</el-button>
+      </common-form>
+    </div>
+
+    <common-table></common-table>
+  </div>
 </template>
 
 <script>
-export default {};
+import CommonForm from "@/components/CommonForm.vue";
+import CommonTable from "@/components/CommonTable.vue";
+
+export default {
+  components: { CommonForm, CommonTable },
+  data() {
+    return {
+      searchForm: {
+        keyword: "",
+      },
+      formLabel: [
+        {
+          model: "keyword",
+          label: "关键词",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
