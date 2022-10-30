953a596f301776dd237dd03f8318fd749da97171
FionaYuliang <FionaYuliang@users.noreply.github.com>
2022-10-30 12:48:35 +0000

    deploy: c3ff81a86626972f70623c912b2618e378496d15

--- src/views/Other/PageOne.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>page one</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style lang="scss" scoped></style>
--- src/views/Other/PageTwo.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>page two</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style lang="scss" scoped></style>
